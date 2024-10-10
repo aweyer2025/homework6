67c73d9c2856c6810256ec880667990d3640b2e1
Anthony <aweyer2021@gmail.com>
2024-10-10 16:04:37 -0400

    done just need to clean up code

--- sentiment.py
+++ sentiment.py
@@ -26,7 +26,7 @@ for file in os.listdir(input_folder_path):
         text = f.read()
 
     # Split text into paragraphs
-    paragraphs = text.split('\t')  # Assuming paragraphs are separated by two newlines
+    paragraphs = text.split('\t')  
 
     # Analyze sentiment for each paragraph
     paragraph_scores = []
@@ -34,7 +34,7 @@ for file in os.listdir(input_folder_path):
         if paragraph.strip():  # Skip empty paragraphs
             scores = analyze_sentiment(paragraph)
             paragraph_scores.append((i, scores))
-            print(f"Paragraph {i + 1} Scores: {scores}")
+            # print(f"Paragraph {i + 1} Scores: {scores}")
 
 
     sentances = text.split('.')
@@ -45,12 +45,26 @@ for file in os.listdir(input_folder_path):
         if sentance.strip():
             scores = analyze_sentiment(sentance)
             sentance_scores.append((i,scores))
-            print(f"Sentance {i + 1} Scores: {scores}")
+            # print(f"Sentance {i + 1} Scores: {scores}")
+
+    document = text
+    documents=" "
+    documentScore = analyze_sentiment(document)
+    print (f"score for document as a whole {documentScore}")
+  
+
+
+
+
 
     # Output sentiment scores for each paragraph
-    output_file_path = os.path.join(output_folder_path, f"analysed_{file}")
+    output_file_path = os.path.join(output_folder_path, f"analysed_{file}\n")
     with open(output_file_path, 'w', encoding='utf-8') as output_file:
+        output_file.write(f"Whole document sentiment score {documentScore}")
         for para_index, scores in paragraph_scores:
             output_file.write(f"Paragraph {para_index + 1} Scores: {scores}\n")
+        for sentance_index, scores in sentance_scores:
+            output_file.write(f"Sentence {sentance_index} Scores: {scores}\n")
+        
 
     print(f"Sentiment analysis completed for file: {file}\n")
